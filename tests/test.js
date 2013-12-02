5d7be91a2d7b618d75953c527144831de65b5b75
Ugnius <kaugnius@gmail.com>
2013-12-02 20:42:11 +0200

    Added mocha tests

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+﻿
+var assert = require('assert');
+var nodeDriver = require('../index.js');
+
+
+suite('index', function() {
+
+	suite('objects', function() {
+		
+		test('should have Cluster object', function() {
+			
+			assert.equal(true, nodeDriver.hasOwnProperty('Cluster'));
+
+		});
+	});
+});
